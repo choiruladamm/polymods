28d7ae94ad8494716bbd11a15fa489ba9f529250
choiruladamm <choiruladamm@gmail.com>
2025-01-18 22:50:55 +0700

    feat: add formatter prettier & .editorconfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import type { Config } from "tailwindcss";
 
 const config: Config = {
     darkMode: ["class"],
     content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./modules/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
